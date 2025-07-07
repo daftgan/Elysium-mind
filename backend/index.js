994fd9797566b1986d8cac9a9579e5ab56b322c1
dagan Remeur <dagan.remeur@gmail.com>
2025-07-07 23:46:51 +0200

    Improve configuration handling

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
